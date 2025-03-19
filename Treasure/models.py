39802b76a8f145db8e249d4b18dd47c0b0f322c7
ZahaRehman <zaharehman788@gmail.com>
2025-03-19 23:04:37 +0500

    Model designe

--- Treasure/models.py
+++ Treasure/models.py
@@ -1,12 +1,22 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, JSON
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from .database import Base
-from sqlalchemy.orm import relationship
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String)
-    password = Column(String)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    progress: Mapped[dict] = mapped_column(JSON, default={"island_1": False, "island_2": False, "treasure": False})
+
+
+class Challenge(Base):
+    __tablename__ = 'challenges'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    island_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    question: Mapped[str] = mapped_column(String, nullable=False)
+    solution: Mapped[str] = mapped_column(String, nullable=False)
